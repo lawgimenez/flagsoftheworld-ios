37eab63b802bd544d707853cc37fce4c56348e57
Lawrence Gimenez <lawgimenez@icloud.com>
2022-10-30 19:04:38 +0800

    Initial commit

---
+++ FlagsOfTheWorld/FlagsOfTheWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagsOfTheWorldApp.swift
+//  FlagsOfTheWorld
+//
+//  Created by Lawrence Gimenez on 10/30/22.
+//
+
+import SwiftUI
+
+@main
+struct FlagsOfTheWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
